c922be40534a20248a46b942d690a33fefd91c40
kurages <kuragebots@gmail.com>
2022-06-16 21:53:24 +0900

    add echo

---
+++ src/main/java/com/example/demo/controller/EchoController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.demo.model.EchoModel;
+
+@Controller
+@RequestMapping("/echo")
+public class EchoController {
+	@ModelAttribute
+	public EchoModel createEchoModel() {
+		EchoModel form = new EchoModel();
+		return form;
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String index(Model model) {
+		return "index";
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String echo(EchoModel form, Model model) {
+		model.addAttribute("val", form.getName());
+		return "echo";
+	}
+}
+
+
---
+++ src/main/java/com/example/demo/model/EchoModel.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+public class EchoForm {
+	private String val;
+
+	public void setVal(String val) {
+		this.val = val;
+	}
+
+	public String getVal() {
+		return val;
+	}
+}
